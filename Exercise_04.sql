d9f60f638a5ef71d418e1fd8307f194dfdf19bf6
Nguyễn Tấn Du-14/09/2004 <du01012004@gmail.com>
2026-04-24 00:58:13 +0700

    Additional analysis and recommendations

--- Exercise_04.sql
+++ Exercise_04.sql
@@ -18,3 +18,11 @@ ALTER TABLE USERS
 MODIFY Phone VARCHAR(15);
 
 SELECT * FROM USERS;
+
+-- Phân tích đề xuất giải pháp
+-- Giải pháp 1: Sửa trực tiếp (Sử dụng ALTER TABLE MODIFY. MySQL tạo bảng tạm, chuyển dữ liệu và ghi đè bảng cũ) => phú hợp hệ thống nhỏ hoặc có thời gian bảo trì
+-- Giải pháp 2: Dùng cột ẩn/ cột phụ (Thêm cột mới VARCHAR, đồng bộ dữ liệu từ cột cũ sang, sau đó đổi tên cột) => Phù hợp hệ thống lớn , yêu cầu online 24/7
+
+-- So sánh
+-- Giải pháp 1 (Direct Alter): Dùng một lệnh sửa trực tiếp; ưu điểm là nhanh, gọn nhưng nhược điểm lớn là gây khóa bảng (Table Lock), dễ làm treo ứng dụng khi có 2 triệu bản ghi
+-- Giải pháp 2 (Shadow Column): Tạo cột tạm rồi mới chuyển dữ liệu; ưu điểm là an toàn, không gián đoạn hệ thống (Zero Downtime) nhưng nhược điểm là thao tác phức tạp, tốn tài nguyên.
